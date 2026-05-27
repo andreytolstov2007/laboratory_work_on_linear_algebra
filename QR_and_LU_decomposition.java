4eaadca02a00b9e1c843eab49e7b7b3654fb7827
andreytolstov2007 <andreytolstov2007@gmail.com>
2026-05-28 00:34:59 +0300

    Update QR_and_LU_decomposition.java
    табличка стала красивее

--- QR_and_LU_decomposition.java
+++ QR_and_LU_decomposition.java
@@ -1,7 +1,7 @@
-import java.util.Scanner;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Random;
+import java.util.Scanner;
 
 public class QR_and_LU_decomposition {
     public static void main(String[] args) {
@@ -9,190 +9,94 @@ public class QR_and_LU_decomposition {
         Random random = new Random();
         int size = 1;
 
-        System.out.println("Размер квадратной матрицы: (время выполнения LU), (время выполнения QR)");
+        System.out.println("Сравнение времени выполнения LU и QR разложения:");
+        System.out.printf("%-25s %-25s %s%n", "Размер матрицы (n x n)", "LU Decomposition (мкс)", "QR Decomposition (мкс)");
+        System.out.println("---------------------------------------------------------------------------------------------------");
 
-        for (int k = 0; k < 4; k ++) {
-            //System.out.print("Введите размерность квадратной матрицы: ");
+        for (int k = 0; k < 4; k++) {
             size *= 5;
 
-            int size_row = size;
-            int size_column = size;
-
+            int n = size;
             byte error;
+            long duration_lu, duration_qr;
 
-            long duration1;
-            long duration2;
+            double[][] A = new double[n][n];
+            double[][] Q = new double[n][n];
+            double[][] R = new double[n][n];
+            double[][] L = new double[n][n];
+            double[][] U = new double[n][n];
 
-            double[][] A = new double[size_row][size_column];
-            double[][] Q = new double[size_row][size_column];
-            double[][] R = new double[size_column][size_column];
-            double[][] L = new double[size][size];
-            double[][] U = new double[size][size];
+            double[] b1 = new double[n];
+            double[] y_for_lu_1 = new double[n];
+            double[] x_from_lu_1 = new double[n];
 
-            double [] b1 = new double[size];
-            double [] b2 = new double[size];
-
-            double [] x_with_LU_1 = new double[size];
-            double [] x_with_LU_2 = new double[size];
-            double [] x_with_QR_1 = new double[size];
-            double [] x_with_QR_2 = new double[size];
-
-            double [] y_with_LU_1 = new double[size];
-            double [] y_with_LU_2 = new double[size];
-            double [] y_with_QR_1 = new double[size];
-            double [] y_with_QR_2 = new double[size];
-
-            //System.out.println("Введите элементы матрицы построчно:");
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    //A[i][j] = scan.nextDouble();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
                     A[i][j] = 15 * random.nextDouble();
                 }
-            }
-
-            //System.out.println("Введите элементы вектора b1 через пробел:");
-            for (int i = 0; i < size; i++) {
-                //b1[i] = scan.nextDouble();
                 b1[i] = 15 * random.nextDouble();
             }
 
-            //System.out.println("Введите элементы вектора b2 через пробел:");
-            for (int i = 0; i < size; i++) {
-                //b2[i] = scan.nextDouble();
-                b2[i] = 15 * random.nextDouble();
-            }
-
-            // Вызов LU-разложения после ввода данных
             long startTime = System.nanoTime();
 
-            error = LU_decomposition(A, L, U, size);
-            if (error == 0) {
-                return ;
+            error = LU_decomposition(A, L, U, n);
+            if (error == 0) { 
+                System.out.println("\nРазложение LU прервано.");
+                break;
             }
 
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j <= i; j++) {
-                    if (j == 0) {
-                        y_with_LU_1[i] = b1[i];
-                    }
-                    else {
-                        y_with_LU_1[i] -= L[i][j-1] * y_with_LU_1[j-1];
-                    }
+            for (int i = 0; i < n; i++) {
+                y_for_lu_1[i] = b1[i];
+                for (int j = 0; j < i; j++) {
+                    y_for_lu_1[i] -= L[i][j] * y_for_lu_1[j];
                 }
             }
 
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j <= i; j++) {
-                    if (j == 0) {
-                        x_with_LU_1[(size - 1) - i] = y_with_LU_1[(size - 1) - i];
-                    }
-                    else {
-                        x_with_LU_1[(size - 1) - i] -= U[(size - 1) - i][(size - 1) - (j - 1)] * x_with_LU_1[(size - 1) - (j - 1)];
-                    }
+            for (int i = n - 1; i >= 0; i--) {
+                x_from_lu_1[i] = y_for_lu_1[i];
+                for (int j = i + 1; j < n; j++) {
+                    x_from_lu_1[i] -= U[i][j] * x_from_lu_1[j];
                 }
-                x_with_LU_1[(size - 1) - i] /= U[(size - 1) - i][(size - 1) - i];
-            }
-
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j <= i; j++) {
-                    if (j == 0) {
-                        y_with_LU_2[i] = b2[i];
-                    }
-                    else {
-                        y_with_LU_2[i] -= L[i][j-1] * y_with_LU_2[j-1];
-                    }
-                }
-            }
-
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j <= i; j++) {
-                    if (j == 0) {
-                        x_with_LU_2[(size - 1) - i] = y_with_LU_2[(size - 1) - i];
-                    }
-                    else {
-                        x_with_LU_2[(size - 1) - i] -= U[(size - 1) - i][(size - 1) - (j - 1)] * x_with_LU_2[(size - 1) - (j - 1)];
-                    }
-                }
-                x_with_LU_2[(size - 1) - i] /= U[(size - 1) - i][(size - 1) - i];
+                x_from_lu_1[i] /= U[i][i];
             }
 
             long endTime = System.nanoTime();
-            duration1 = (endTime - startTime) / 1000;
+            duration_lu = (endTime - startTime) / 1000;
 
             startTime = System.nanoTime();
 
-            error = QR_decomposition(A, Q, R, size_row, size_column);
+            error = QR_decomposition(A, Q, R, n, n);
             if (error == 0) {
-                return ;
+                System.out.println("\nРазложение QR прервано.");
+                break;
             }
 
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    y_with_QR_1[i] += Q[j][i] * b1[j];
+            double[] y_for_qr_1 = new double[n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    y_for_qr_1[i] += Q[j][i] * b1[j];
                 }
             }
 
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j <= i; j++) {
-                    if (j == 0) {
-                        x_with_QR_1[(size - 1) - i] = y_with_QR_1[(size - 1) - i];
-                    }
-                    else {
-                        x_with_QR_1[(size - 1) - i] -= R[(size - 1) - i][(size - 1) - (j - 1)] * x_with_QR_1[(size - 1) - (j - 1)];
-                    }
+            double[] x_from_qr_1 = new double[n];
+            for (int i = n - 1; i >= 0; i--) {
+                x_from_qr_1[i] = y_for_qr_1[i];
+                for (int j = i + 1; j < n; j++) {
+                    x_from_qr_1[i] -= R[i][j] * x_from_qr_1[j];
                 }
-                x_with_QR_1[(size - 1) - i] /= R[(size - 1) - i][(size - 1) - i];
+                x_from_qr_1[i] /= R[i][i];
             }
 
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    y_with_QR_2[i] += Q[j][i] * b2[j];
-                }
-            }
-
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j <= i; j++) {
-                    if (j == 0) {
-                        x_with_QR_2[(size - 1) - i] = y_with_QR_2[(size - 1) - i];
-                    }
-                    else {
-                        x_with_QR_2[(size - 1) - i] -= R[(size - 1) - i][(size - 1) - (j - 1)] * x_with_QR_2[(size - 1) - (j - 1)];
-                    }
-                }
-                x_with_QR_2[(size - 1) - i] /= R[(size - 1) - i][(size - 1) - i];
-            }
-            // Вывод результатов
             endTime = System.nanoTime();
-            duration2 = (endTime - startTime) / 1000;
+            duration_qr = (endTime - startTime) / 1000;
 
-            // System.out.println();
-            // System.out.println("Матрица U:");
-            // Print_matrix(U, size, size);
-            
-            // System.out.println("Матрица L:");
-            // Print_matrix(L, size, size);
-
-            // System.out.println("Матрица Q:");
-            // Print_matrix(Q, size_row, size_column);
-
-            // System.out.println("Матрица R:");
-            // Print_matrix(R, size_column, size_column);
-
-            // Print_vector(x_with_LU_1, size);
-            // Print_vector(x_with_LU_2, size);
-            
-            // Print_vector(x_with_QR_1, size);
-            // Print_vector(x_with_QR_2, size);
-
-            System.out.println(size +": "+ duration1 + " мкс, " + duration2 + " мкс");
+            System.out.printf("%-25d %-25d %d%n", n, duration_lu, duration_qr);
         }
 
-            scan.close();
+        scan.close();
     }
 
     public static byte LU_decomposition(double[][] A, double[][] L, double[][] U, int size) {
-
-        // Копируем A в рабочую матрицу M
         double[][] M = new double[size][size];
         for (int i = 0; i < size; i++) {
             System.arraycopy(A[i], 0, M[i], 0, size);
@@ -200,28 +104,24 @@ public class QR_and_LU_decomposition {
 
         for (int k = 0; k < size; k++) {
             if (M[k][k] == 0) {
-                System.out.println("Данную матрицу разложить таким образом невозможно.");
+                System.out.println("Данную матрицу разложить таким образом (LU) невозможно.");
                 return 0;
             }
 
-            // Формируем строку U[k][*]
             for (int j = k; j < size; j++) {
                 U[k][j] = M[k][j];
             }
 
-            // Формируем столбец L[*][k]
             for (int i = k; i < size; i++) {
                 L[i][k] = M[i][k] / U[k][k];
             }
 
-            // Обновляем рабочую матрицу M
             for (int i = k + 1; i < size; i++) {
                 for (int j = k + 1; j < size; j++) {
                     M[i][j] = M[i][j] - L[i][k] * U[k][j];
                 }
             }
         }
-
         return 1;
     }
 
@@ -232,54 +132,33 @@ public class QR_and_LU_decomposition {
                     for (int k = 0; k < size_row; k++) {
                         Q[k][i] = A[k][i];
                     }
-                }
-                else {
+                } else {
                     for (int k = 0; k < size_row; k++) {
                         R[j-1][i] += Q[k][j-1] * A[k][i];
                     }
                     for (int k = 0; k < size_row; k++) {
-                        Q[k][i] -= (R[j-1][i] * Q[k][j-1]);
+                        Q[k][i] -= R[j-1][i] * Q[k][j-1];
                     }
                 }
             }
             R[i][i] = Find_sqrt(Q, size_row, i);
             if (R[i][i] == 0) {
-                System.out.println("Данная матрица не раскладывается на Q и R");
+                System.out.println("Данная матрица не раскладывается на Q и R.");
                 return 0;
             }
             for (int k = 0; k < size_row; k++) {
                 Q[k][i] /= R[i][i];
             }
         }
-
         return 1;
     }
 
-    public static void Print_matrix(double[][] matrix, int size_row, int size_column) {
-        for (int i = 0; i < size_row; i++) {
-            for (int j = 0; j < size_column; j++) {
-                System.out.printf("%.5f ", matrix[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println();
-
-    }
-
-    public static void Print_vector(double[] vector, int size) {
-        for (int i = 0; i < size; i++) {
-            System.out.printf("%.5f ", vector[i]);
-        }
-        System.out.println();
-    }
-
-    // функция для подсчёта корня от суммы квадратов
     public static double Find_sqrt(double[][] matrix, int size_row, int column) {
         double sum = 0;
         for (int i = 0; i < size_row; i++) {
             BigDecimal bd = new BigDecimal(Double.toString(matrix[i][column] * matrix[i][column]));
-            bd = bd.setScale(25, RoundingMode.HALF_UP); //Округление с точночтью до 25 знаков после запятой.
-            sum += (bd.doubleValue());
+            bd = bd.setScale(25, RoundingMode.HALF_UP);
+            sum += bd.doubleValue();
         }
         return Math.sqrt(sum);
     }
